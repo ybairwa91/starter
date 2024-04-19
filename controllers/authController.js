5e06f9dcd5333ea1f2597f2361bd1008f7d1ee16
Yogeshbairwa <ybairwa786@gmail.com>
2024-04-19 17:21:17 +0530

    UPDATEs

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
+//////////////////////////////////////////////////
+//learning to sign up
+/*
+
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
@@ -14,6 +19,39 @@ const catchAsync = require('../utils/catchAsync');
 exports.signup = catchAsync(async (req, res, next) => {
   //game of mongoose
   const newUser = await User.create(req.body);
+  res.status(201).json({
+    status: 'success',
+    data: {
+      user: newUser,
+    },
+  });
+});
+
+*/
+
+///////////////////////////////////////
+
+//AUTHENTICATION
+
+// 1) More realistic user signup details
+// 2) install jsonwebtoken npm
+// 3) read its documentation
+// 4) import it
+
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = jwt.sign({ id: newUser._id }, 'secret');
+
   res.status(201).json({
     status: 'success',
     data: {
